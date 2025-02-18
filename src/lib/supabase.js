578774a3f3bd21b13049c7dac35d59396880dc75
Tyler <tymurrayco@gmail.com>
2025-02-18 13:26:26 -0700

    Update login rediect url

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://tjfojodrdewgakxxmaue.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqZm9qb2RyZGV3Z2FreHhtYXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5MDY0MjQsImV4cCI6MjA1NTQ4MjQyNH0._aBs3MrFkJr3A2OeFLHdmKphDROMeLkdc8KMm61EeC0'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    redirectTo: 'https://extra-services-meeting.vercel.app',
+    persistSession: true,
+    autoRefreshToken: true
+  }
+})
