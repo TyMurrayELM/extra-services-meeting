bde0ca7de0563a36185759621de1972b29f06349
Tyler <tymurrayco@gmail.com>
2025-02-18 16:22:51 -0700

    Still trying to fix SSO redirect

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://extra-services-meeting.vercel.app',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
